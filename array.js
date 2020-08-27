600a2a3356e6920038c0ce97cb56510f35cb7dc5
Evgeniy Kolominskiy <kolominskiyy@gmail.com>
2020-08-27 12:01:19 +0300

    Initial commit.
    Add array.js.

---
+++ array.js
@@ -0,0 +1,123 @@
+'use strict';
+
+function MyArray() {
+	if (!new.target) {
+		return new MyArray();
+	}
+	this.length = 0;
+	this.isMyArray = function isMyArray() {
+		return (this instanceof MyArray);
+	}
+}
+
+const myArrProto = new MyArray();
+MyArray.prototype = myArrProto;
+
+myArrProto.push = function push() {
+	for (let i = 0; i < arguments.length; i++) {
+		this[this.length++] = arguments[i];
+	}
+	return this.length;
+};
+
+myArrProto.find = function find(callback) {
+	let element;
+	for (let i = 0; i < this.length; i++) {
+		if (callback(this[i], i, this)) {
+			element = this[i];
+			break;
+		}
+	}
+	return element;
+};
+
+myArrProto.includes = function includes(valueToFind, fromIndex = 0) {
+	if (fromIndex < 0) {
+		fromIndex = this.length + fromIndex;
+	}
+	for (let i = fromIndex; i < this.length; i++) {
+		if (this[i] === valueToFind) {
+			return true;
+		}
+	}
+	return false;
+}
+
+myArrProto.join = function join(separator = ',') {
+	let string = '';
+	for (let i = 0; i < this.length - 1; i++) {
+		string = string + String(this[i]) + separator;
+	}
+	string = string + String(this[this.length - 1]);
+	return string;
+}
+
+myArrProto.filter = function filter(callback) {
+	const myArr = new MyArray();
+	for (let i = 0; i < this.length; i++) {
+		if (callback(this[i], i, this)) {
+			myArr.push(this[i]);
+		}
+	}
+	return myArr;
+}
+
+myArrProto.map = function map(callback) {
+	const myArr = new MyArray();
+	for (let i = 0; i < this.length; i++) {
+		myArr.push(callback(this[i], i, this));
+	}
+	return myArr;
+}
+
+myArrProto.reduce = function reduce(callback, initialValue) {
+	let counterStart;
+	let accumulator;
+	if (initialValue === undefined) {
+		counterStart = 1;
+		accumulator = this[0];
+	} else {
+		counterStart = 0;
+		accumulator = initialValue;
+	}
+	for (let i = counterStart; i < this.length; i++) {
+		accumulator = callback(accumulator, this[i], i, this);
+	}
+	return accumulator;
+}
+
+myArrProto.pop = function pop() {
+	const lastElement = this[this.length - 1];
+	delete this[this.length - 1];
+	this.length--;
+	return lastElement;
+}
+
+myArrProto.concat = function concat() {
+	const newArr = new this.constructor();
+	for (let i = 0; i < this.length; i++) {
+		newArr.push(this[i]);
+	}
+	for (let i = 0; i < arguments.length; i++) {
+		if (arguments[i] instanceof MyArray) {
+			for (let j = 0; j < arguments[i].length; j++) {
+				newArr.push(arguments[i][j]);
+			}
+			continue;
+		}
+		newArr.push(arguments[i]);
+	}
+	return newArr;
+};
+
+myArrProto.flat = function flat(depth = 1) {
+
+	let myArray = new this.constructor();
+	for (let i = 0; i < this.length; i++) {
+		myArray = myArray.concat(this[i]);
+	}
+	if (--depth > 0) {
+		myArray = myArray.flat();
+	}
+	return myArray;
+}
--- index.js
+++ index.js
@@ -1,2 +1,49 @@
 'use strict';
 
+const customArray = new MyArray();
+console.log(`Output of "customArray.isMyArray()": ${customArray.isMyArray()}`);
+
+customArray.push(10, 7, 8, 'a', 'b');
+console.log(customArray);
+
+const arr = [10, 7, 8, 'a', 'b'];
+
+function isOdd(number) {
+	return number % 2 !== 0;
+}
+
+console.log(arr.find(isOdd));
+console.log(customArray.find(isOdd));
+
+console.log(arr.includes('a', -2));
+console.log(customArray.includes('a', -2));
+
+console.log(arr.join(''));
+console.log(customArray.join(''));
+
+console.log(arr.filter(isOdd));
+console.log(customArray.filter(isOdd));
+
+function addOne(element) {
+	return element + "1";
+}
+
+console.log(arr.map(addOne));
+console.log(customArray.map(addOne));
+
+function reducer(accumulator, currentValue) {
+	return accumulator + currentValue;
+}
+
+console.log(arr.reduce(reducer, 0));
+console.log(customArray.reduce(reducer, 0));
+
+console.log(customArray.pop());
+console.log(customArray);
+
+const customArray2 = new MyArray();
+customArray2.push('f', 'd', customArray);
+const customArray3 = new MyArray();
+customArray3.push(33, 88, customArray2, customArray);
+
+console.log(customArray3.flat(2));
